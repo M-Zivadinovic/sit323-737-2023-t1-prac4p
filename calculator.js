7eb68ee67ce15f011d364ada478da5a0aff46310
alamfaisal654 <alamfaisal654@gmail.com>
2023-03-31 21:12:51 +1100

    First commit

---
+++ calculator.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const add = (n1, n2) => {
+  return n1 + n2;
+};
+const sub = (n1, n2) => {
+  return n1 - n2;
+};
+const mult = (n1, n2) => {
+  return n1 * n2;
+};
+const div = (n1, n2) => {
+  return n1 / n2;
+};
+app.get("/add", (req, res) => {
+  try {
+    const n1 = parseFloat(req.query.n1);
+    const n2 = parseFloat(req.query.n2);
+    if (isNaN(n1)) {
+      throw new Error("n1 incorrectly defined");
+    }
+    if (isNaN(n2)) {
+      throw new Error("n2 incorrectly defined");
+    }
+    const result = add(n1, n2);
+    res.status(200).json({ statuscode: 200, data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ statuscode: 500, msg: error.toString() });
+  }
+});
+app.get("/sub", (req, res) => {
+  try {
+    const n1 = parseFloat(req.query.n1);
+    const n2 = parseFloat(req.query.n2);
+    if (isNaN(n1)) {
+      throw new Error("n1 incorrectly defined");
+    }
+    if (isNaN(n2)) {
+      throw new Error("n2 incorrectly defined");
+    }
+    const result = sub(n1, n2);
+    res.status(200).json({ statuscode: 200, data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ statuscode: 500, msg: error.toString() });
+  }
+});
+app.get("/mult", (req, res) => {
+  try {
+    const n1 = parseFloat(req.query.n1);
+    const n2 = parseFloat(req.query.n2);
+    if (isNaN(n1)) {
+      throw new Error("n1 incorrectly defined");
+    }
+    if (isNaN(n2)) {
+      throw new Error("n2 incorrectly defined");
+    }
+    const result = mult(n1, n2);
+    res.status(200).json({ statuscode: 200, data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ statuscode: 500, msg: error.toString() });
+  }
+});
+app.get("/div", (req, res) => {
+  try {
+    const n1 = parseFloat(req.query.n1);
+    const n2 = parseFloat(req.query.n2);
+    if (isNaN(n1)) {
+      throw new Error("n1 incorrectly defined");
+    }
+    if (isNaN(n2)) {
+      throw new Error("n2 incorrectly defined");
+    }
+    const result = div(n1, n2);
+    res.status(200).json({ statuscode: 200, data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ statuscode: 500, msg: error.toString() });
+  }
+});
+const port = 3040;
+app.listen(port, () => {
+  console.log("hello im listenting to port" + port);
+});
